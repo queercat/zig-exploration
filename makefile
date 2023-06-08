d5da7674b463b053e04622a149fe56b90e9bc762
May Tusek <tusek.may@gmail.com>
2023-06-07 23:53:24 -0700

    Adds vectors.

--- makefile
+++ makefile
@@ -1,3 +1,7 @@
-all:
+hello:
 	zig build-exe src/hello-world.zig -femit-bin=./build/hello-world
 	./build/hello-world
+
+vectors:
+	zig build-exe src/vectors.zig -femit-bin=./build/vectors
+	./build/vectors
---
+++ src/vectors.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+
+pub fn main() !void {
+  const allocator = std.heap.page_allocator;
+  var list = ArrayList(u64).init(allocator);
+  defer list.deinit();
+  for (0..10) |i| {
+    try list.append(@as(u64, i));
+  }
+  for (list.items) |item| {
+    std.debug.print("{}", .{item});
+  }
+}
